2c440c49831b4c6e454a3593986fa500a5067bad
DaigoTaguchi <daigo.taguchi@jp.ricoh.com>
2025-02-19 16:48:26 +0900

    match ページを追加

---
+++ src/app/team/layout.tsx
@@ -0,0 +1,11 @@
+export default function TeamLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="max-w-2xl mx-auto">
+      <div className="bg-white rounded-xl shadow sm:p-7">{children}</div>
+    </div>
+  );
+}
