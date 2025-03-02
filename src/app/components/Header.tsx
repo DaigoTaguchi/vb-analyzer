791b5e4b006a96225265bcea783272c7d4a0f82c
taguchi <32448229+DaigoTaguchi@users.noreply.github.com>
2025-03-02 17:10:16 +0900

    時計を追加 (#5)

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import Clock from "./Clock";
 
 export default function Header() {
   return (
     <header className="bg-gray-900 text-white w-full">
-      <div className="w-full py-2 px-4 flex justify-start">
+      <div className="w-full py-2 px-4 flex justify-between items-center">
         <Link href="/">
           <Image
             src="/logo1.png"
@@ -14,6 +15,7 @@ export default function Header() {
             priority
           ></Image>
         </Link>
+        <Clock />
       </div>
     </header>
   );
